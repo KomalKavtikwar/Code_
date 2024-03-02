11ea8303b0ace546ffb50fdef778f4cbc4002cb0
KomalKavtikwar <126814588+KomalKavtikwar@users.noreply.github.com>
2024-03-02 12:52:50 +0530

    Add files via upload

---
+++ CPP_/Area_of_triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b,h,area;
+    cout<<"Enter the base : ";
+    cin>>b;
+    cout<<"Enter the height :";
+    cin>>h;
+
+    area=(b*h)/2;
+    cout<<"Area of triangle is : "<<area;
+    return 0;
+}
